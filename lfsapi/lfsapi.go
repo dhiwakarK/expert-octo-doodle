bk2204/git-lfs

lfsapi: move SSHTransfer instantiation to lfsapi client
The lfsapi client is used to perform operations for SSH authentication
already, so we know we'll have one wherever SSH operations might be
done.  Let's move the instantiation to the client so that we can reuse
it both in the transfer queue code and in the locking code as well.

lfsapi/lfsapi.go
@@ -6,6 +6,8 @@ import (
 	"github.com/git-lfs/git-lfs/creds"
 	"github.com/git-lfs/git-lfs/errors"
 	"github.com/git-lfs/git-lfs/lfshttp"
+	"github.com/git-lfs/git-lfs/ssh"
+	"github.com/rubyist/tracerx"
 )
 
 type Client struct {
@@ -43,4 +45,22 @@ func NewClient(ctx lfshttp.Context) (*Client, error) {
 
 func (c *Client) Context() lfshttp.Context {
 	return c.context
+}
+
+// SSHTransfer returns either an suitable transfer object or nil if the
+// server is not using an SSH remote or the git-lfs-transfer style of SSH
+// remote.
+func (c *Client) SSHTransfer(operation, remote string) *ssh.SSHTransfer {
+	endpoint := c.Endpoints.RemoteEndpoint(operation, remote)
+	if len(endpoint.SSHMetadata.UserAndHost) > 0 {
+		ctx := c.Context()
+		tracerx.Printf("attempting pure SSH protocol connection")
+		sshTransfer, err := ssh.NewSSHTransfer(ctx.OSEnv(), ctx.GitEnv(), &endpoint.SSHMetadata, operation)
+		if err != nil {
+			tracerx.Printf("pure SSH protocol connection failed: %s", err)
+			return nil
+		}
+		return sshTransfer
+	}
+	return nil
 }

dhiwakarK/expert-octo-doodle

Former-commit-id: 31d3fb7ceeef219bc7ee466d5938306000de052d

lfsapi/lfsapi.go
@@ -6,6 +6,8 @@ import (
 	"github.com/git-lfs/git-lfs/creds"
 	"github.com/git-lfs/git-lfs/errors"
 	"github.com/git-lfs/git-lfs/lfshttp"
+	"github.com/git-lfs/git-lfs/ssh"
+	"github.com/rubyist/tracerx"
 )
 
 type Client struct {
@@ -43,4 +45,22 @@ func NewClient(ctx lfshttp.Context) (*Client, error) {
 
 func (c *Client) Context() lfshttp.Context {
 	return c.context
+}
+
+// SSHTransfer returns either an suitable transfer object or nil if the
+// server is not using an SSH remote or the git-lfs-transfer style of SSH
+// remote.
+func (c *Client) SSHTransfer(operation, remote string) *ssh.SSHTransfer {
+	endpoint := c.Endpoints.Endpoint(operation, remote)
+	if len(endpoint.SSHMetadata.UserAndHost) > 0 {
+		ctx := c.Context()
+		tracerx.Printf("attempting pure SSH protocol connection")
+		sshTransfer, err := ssh.NewSSHTransfer(ctx.OSEnv(), ctx.GitEnv(), &endpoint.SSHMetadata, operation)
+		if err != nil {
+			tracerx.Printf("pure SSH protocol connection failed: %s", err)
+			return nil
+		}
+		return sshTransfer
+	}
+	return nil
 }