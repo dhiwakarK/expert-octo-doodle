bk2204/git-lfs

tq: turn the batch client into an interface
Right now, we always perform a batch operation using an instance of the
transfer queue client, which is HTTP based.  However, in the future,
we'll want to add an SSH-based option, so let's turn the client into a
simple interface we can use to abstract this away.

tq/api.go
@@ -11,7 +11,7 @@ import (
 )
 
 type tqClient struct {
-	MaxRetries int
+	maxRetries int
 	*lfsapi.Client
 }
 
@@ -32,6 +32,12 @@ type BatchResponse struct {
 	endpoint            lfshttp.Endpoint
 }
 
+type BatchTransferAdapter interface {
+	Batch(remote string, bReq *batchRequest) (*BatchResponse, error)
+	MaxRetries() int
+	SetMaxRetries(n int)
+}
+
 func Batch(m *Manifest, dir Direction, remote string, remoteRef *git.Ref, objects []*Transfer) (*BatchResponse, error) {
 	if len(objects) == 0 {
 		return &BatchResponse{}, nil
@@ -43,6 +49,14 @@ func Batch(m *Manifest, dir Direction, remote string, remoteRef *git.Ref, object
 		TransferAdapterNames: m.GetAdapterNames(dir),
 		Ref:                  &batchRef{Name: remoteRef.Refspec()},
 	})
+}
+
+func (c *tqClient) MaxRetries() int {
+	return c.maxRetries
+}
+
+func (c *tqClient) SetMaxRetries(n int) {
+	c.maxRetries = n
 }
 
 func (c *tqClient) Batch(remote string, bReq *batchRequest) (*BatchResponse, error) {
@@ -71,7 +85,7 @@ func (c *tqClient) Batch(remote string, bReq *batchRequest) (*BatchResponse, err
 	tracerx.Printf("api: batch %d files", len(bReq.Objects))
 
 	req = c.Client.LogRequest(req, "lfs.batch")
-	res, err := c.DoAPIRequestWithAuth(remote, lfshttp.WithRetries(req, c.MaxRetries))
+	res, err := c.DoAPIRequestWithAuth(remote, lfshttp.WithRetries(req, c.MaxRetries()))
 	if err != nil {
 		tracerx.Printf("api error: %s", err)
 		return nil, errors.Wrap(err, "batch response")

tq/manifest.go
@@ -30,7 +30,7 @@ type Manifest struct {
 	uploadAdapterFuncs      map[string]NewAdapterFunc
 	fs                      *fs.Filesystem
 	apiClient               *lfsapi.Client
-	tqClient                *tqClient
+	batchClientAdapter      BatchTransferAdapter
 	mu                      sync.Mutex
 }
 
@@ -54,11 +54,11 @@ func (m *Manifest) IsStandaloneTransfer() bool {
 	return m.standaloneTransferAgent != ""
 }
 
-func (m *Manifest) batchClient() *tqClient {
+func (m *Manifest) batchClient() BatchTransferAdapter {
 	if r := m.MaxRetries(); r > 0 {
-		m.tqClient.MaxRetries = r
+		m.batchClientAdapter.SetMaxRetries(r)
 	}
-	return m.tqClient
+	return m.batchClientAdapter
 }
 
 func NewManifest(f *fs.Filesystem, apiClient *lfsapi.Client, operation, remote string) *Manifest {
@@ -74,7 +74,7 @@ func NewManifest(f *fs.Filesystem, apiClient *lfsapi.Client, operation, remote s
 	m := &Manifest{
 		fs:                   f,
 		apiClient:            apiClient,
-		tqClient:             &tqClient{Client: apiClient},
+		batchClientAdapter:   &tqClient{Client: apiClient},
 		downloadAdapterFuncs: make(map[string]NewAdapterFunc),
 		uploadAdapterFuncs:   make(map[string]NewAdapterFunc),
 	}

dhiwakarK/expert-octo-doodle

Former-commit-id: 3e5172123e20a8fe138e933e40314a2a68154cc1

tq/api.go
@@ -11,7 +11,7 @@ import (
 )
 
 type tqClient struct {
-	MaxRetries int
+	maxRetries int
 	*lfsapi.Client
 }
 
@@ -45,6 +45,20 @@ func Batch(m *Manifest, dir Direction, remote string, remoteRef *git.Ref, object
 	})
 }
 
+type BatchClient interface {
+	Batch(remote string, bReq *batchRequest) (*BatchResponse, error)
+	MaxRetries() int
+	SetMaxRetries(n int)
+}
+
+func (c *tqClient) MaxRetries() int {
+	return c.maxRetries
+}
+
+func (c *tqClient) SetMaxRetries(n int) {
+	c.maxRetries = n
+}
+
 func (c *tqClient) Batch(remote string, bReq *batchRequest) (*BatchResponse, error) {
 	bRes := &BatchResponse{}
 	if len(bReq.Objects) == 0 {
@@ -71,7 +85,7 @@ func (c *tqClient) Batch(remote string, bReq *batchRequest) (*BatchResponse, err
 	tracerx.Printf("api: batch %d files", len(bReq.Objects))
 
 	req = c.Client.LogRequest(req, "lfs.batch")
-	res, err := c.DoAPIRequestWithAuth(remote, lfshttp.WithRetries(req, c.MaxRetries))
+	res, err := c.DoAPIRequestWithAuth(remote, lfshttp.WithRetries(req, c.MaxRetries()))
 	if err != nil {
 		tracerx.Printf("api error: %s", err)
 		return nil, errors.Wrap(err, "batch response")

tq/manifest.go
@@ -30,7 +30,7 @@ type Manifest struct {
 	uploadAdapterFuncs      map[string]NewAdapterFunc
 	fs                      *fs.Filesystem
 	apiClient               *lfsapi.Client
-	tqClient                *tqClient
+	batchClientAdapter      BatchClient
 	mu                      sync.Mutex
 }
 
@@ -54,11 +54,11 @@ func (m *Manifest) IsStandaloneTransfer() bool {
 	return m.standaloneTransferAgent != ""
 }
 
-func (m *Manifest) batchClient() *tqClient {
+func (m *Manifest) batchClient() BatchClient {
 	if r := m.MaxRetries(); r > 0 {
-		m.tqClient.MaxRetries = r
+		m.batchClientAdapter.SetMaxRetries(r)
 	}
-	return m.tqClient
+	return m.batchClientAdapter
 }
 
 func NewManifest(f *fs.Filesystem, apiClient *lfsapi.Client, operation, remote string) *Manifest {
@@ -74,7 +74,7 @@ func NewManifest(f *fs.Filesystem, apiClient *lfsapi.Client, operation, remote s
 	m := &Manifest{
 		fs:                   f,
 		apiClient:            apiClient,
-		tqClient:             &tqClient{Client: apiClient},
+		batchClientAdapter:   &tqClient{Client: apiClient},
 		downloadAdapterFuncs: make(map[string]NewAdapterFunc),
 		uploadAdapterFuncs:   make(map[string]NewAdapterFunc),
 	}